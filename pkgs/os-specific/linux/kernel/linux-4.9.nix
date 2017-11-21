09df34872992f5f3e264b9620ec42ffa97bd7779
Jonas Schwabe <jonas@jnugh.de>
2017-11-21 11:30:40 +0100

    linux: 4.9.63 -> 4.9.64

--- pkgs/os-specific/linux/kernel/linux-4.9.nix
+++ pkgs/os-specific/linux/kernel/linux-4.9.nix
@@ -1,11 +1,11 @@
 { stdenv, hostPlatform, fetchurl, perl, buildLinux, ... } @ args:
 
 import ./generic.nix (args // rec {
-  version = "4.9.63";
+  version = "4.9.64";
   extraMeta.branch = "4.9";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "18rfvzsxgjr9223r3lznbrj6fh533d68nizpcz556d7x6dpkij91";
+    sha256 = "19627021bdcaeb1046ad0054b80e99e33fa3fc57d20f39bc147df3e86d3830c0";
   };
 } // (args.argsOverride or {}))
